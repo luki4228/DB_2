5f229b22d3bbb106d107ef96e1c4379b99f3602a
luki4228 <zhenyalushik@gmail.com>
2019-09-26 12:39:45 +0300

    Lab4

---
+++ Lab4_651006/V8_1.sql
@@ -0,0 +1,92 @@
+﻿use AdventureWorks2012;
+go
+/*
+a) Создание таблицы
+*/
+
+create table Person.CountryRegionHst (
+	ID int identity(1,1) primary key,
+	Action nvarchar(6) not null check (Action in ('INSERT', 'UPDATE', 'DELETE')),
+	ModifiedDate datetime not null default GETDATE(),
+	SourceID nvarchar(3) not null,
+	UserName nvarchar(100) not null default USER_NAME()
+);
+go
+/*
+b) Создание AFTER триггеров
+*/
+create trigger Person.CountryRegion_INSERT
+on Person.CountryRegion
+after insert 
+as
+	insert into Person.CountryRegionHst (
+		Action,
+		SourceID)
+	values (
+		'INSERT',
+		(select 
+			CountryRegionCode 
+		from 
+			inserted));	
+go
+
+create trigger Person.CountryRegion_UPDATE
+on Person.CountryRegion
+after update 
+as
+	insert into Person.CountryRegionHst (
+		Action,
+		SourceID)
+	values (
+		'UPDATE',
+		(select 
+			CountryRegionCode 
+		from 
+			inserted));
+go
+
+create trigger Person.CountryRegion_DELETE
+on Person.CountryRegion
+after delete 
+as
+	insert into Person.CountryRegionHst (
+		Action,
+		SourceID)
+	values (
+		'DELETE',
+		(select 
+			CountryRegionCode 
+		from 
+			deleted));
+go
+/*
+c) Создание вьюшки
+*/
+create view Person.CountryRegionView
+with encryption
+as 
+	(select * from Person.CountryRegion);
+go
+/*
+d) Проверка триггеров
+*/
+insert into Person.CountryRegionView (
+	CountryRegionCode, 
+	Name, 
+	ModifiedDate)
+values (
+	'ZZ',
+	'Zanzibar',
+	getdate())
+
+update Person.CountryRegionView
+set
+	Name = 'ZombiZanuda'
+where
+	CountryRegionCode = 'ZZ' 
+
+delete Person.CountryRegionView
+where
+	CountryRegionCode = 'ZZ'
+
+select * from Person.CountryRegionHst
---
+++ Lab4_651006/V8_2.sql
@@ -0,0 +1,161 @@
+﻿use AdventureWorks2012;
+go
+/*
+a) Создание вьюшки
+*/
+create view Person.CountryRegion_View
+with schemabinding
+as 
+	(select  
+		CR.CountryRegionCode,
+		CR.Name as Country,
+		ST.CostLastYear,
+		ST.CostYTD,
+		ST.[Group],
+		ST.ModifiedDate,
+		ST.Name,
+		ST.rowguid,
+		ST.SalesLastYear,
+		ST.SalesYTD,
+		ST.TerritoryID
+	from 
+		Person.CountryRegion as CR inner join Sales.SalesTerritory as ST
+	on 
+		CR.CountryRegionCode = ST.CountryRegionCode);
+go
+/*
+a) Создание кластерного индекса
+*/
+create unique clustered index IDX_TerritoryID
+on
+	Person.CountryRegion_View (TerritoryID);
+go
+/*
+b) Создание INSTEAD OF триггера
+*/
+create trigger Person.CountryRegion_INSTEADOFALL
+on Person.CountryRegion_View
+instead of insert, update, delete
+as
+begin
+	if exists (select * from inserted)
+	begin
+		if not exists (
+			select * from inserted as i inner join Person.CountryRegion_View as CRV
+			on 
+				i.TerritoryID = CRV.TerritoryID)
+		begin	
+			insert into Person.CountryRegion
+				(CountryRegionCode,
+				Name,
+				ModifiedDate)
+			select 
+				CountryRegionCode,
+				Country,
+				ModifiedDate
+			from 
+				inserted	
+						
+			insert into Sales.SalesTerritory
+				(CostLastYear,
+				CostYTD,
+				CountryRegionCode,
+				[Group],
+				ModifiedDate,
+				Name,
+				rowguid,
+				SalesLastYear,
+				SalesYTD)
+			select 
+				i.CostLastYear,
+				i.CostYTD,
+				i.CountryRegionCode,
+				i.[Group],
+				i.ModifiedDate,
+				i.Name,
+				i.rowguid,
+				i.SalesLastYear,
+				i.SalesYTD
+			from 
+				inserted as i inner join Person.CountryRegion as CR 
+			on
+				i.CountryRegionCode = CR.CountryRegionCode;			
+		end
+		else
+		begin
+			update Person.CountryRegion
+			set
+				CountryRegionCode = inserted.CountryRegionCode,
+				Name = inserted.Country,
+				ModifiedDate = inserted.ModifiedDate
+			from inserted
+			where inserted.CountryRegionCode = Person.CountryRegion.CountryRegionCode;
+
+			update Sales.SalesTerritory
+			set
+				CostLastYear = inserted.CostLastYear,
+				CostYTD = inserted.CostYTD,
+				CountryRegionCode = inserted.CountryRegionCode,
+				[Group] = inserted.[Group],
+				ModifiedDate = inserted.ModifiedDate,
+				Name = inserted.Name,
+				rowguid = inserted.rowguid,
+				SalesLastYear = inserted.SalesLastYear,
+				SalesYTD = inserted.SalesYTD
+			from inserted
+			where inserted.TerritoryID = Sales.SalesTerritory.TerritoryID;
+		end
+	end
+	if exists (select * from deleted) and not exists (select * from inserted)
+	begin
+		delete Sales.SalesTerritory
+		where TerritoryID in (select TerritoryID from deleted)
+		
+		delete Person.CountryRegion
+		where CountryRegionCode in (select CountryRegionCode from deleted) and CountryRegionCode not in (select CountryRegionCode from Sales.SalesTerritory)
+	end
+end;
+go
+/*
+c) Проверка работы триггера
+*/
+insert into Person.CountryRegion_View (
+	CostLastYear,
+	CostYTD,
+	Country,
+	CountryRegionCode,
+	[Group],
+	ModifiedDate,
+	Name,
+	rowguid,
+	SalesLastYear,
+	SalesYTD)
+values 
+	(0, 0, 'Zanzibar', 'ZZ', 'Zazibar', getdate(), 'Zanzibar', newid(), 13123123, 24343242);
+
+select * from Person.CountryRegion
+where Person.CountryRegion.CountryRegionCode = 'ZZ'
+
+select * from Sales.SalesTerritory
+where Sales.SalesTerritory.CountryRegionCode = 'ZZ'
+
+update Person.CountryRegion_View
+set 
+	CostLastYear = 2,
+	Country = 'Zazibar'
+where 
+	CountryRegionCode = 'ZZ'
+
+select * from Person.CountryRegion
+where Person.CountryRegion.CountryRegionCode = 'ZZ'
+
+select * from Sales.SalesTerritory
+where Sales.SalesTerritory.CountryRegionCode = 'ZZ'
+
+delete Person.CountryRegion_View
+where 
+	CountryRegionCode = 'ZZ'
+
+select * from Person.CountryRegion
+
+select * from Sales.SalesTerritory
