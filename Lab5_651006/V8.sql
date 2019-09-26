5d07c32be34fee2ec934e01ef8cb65a2d0af2698
luki4228 <zhenyalushik@gmail.com>
2019-09-26 14:12:26 +0300

    Lab5

---
+++ Lab5_651006/V8.sql
@@ -0,0 +1,68 @@
+﻿use AdventureWorks2012;
+go
+/*
+Создание scalar-valued функции
+*/
+create function Production.ProductsCount (
+	@id int)
+returns int 
+as
+begin 
+	declare @res int
+	set @res = (
+		select 
+			count(ProductID) 
+		from 
+			Production.Product 
+		where 
+			ProductSubcategoryID = @id)
+	return @res
+end;
+go
+/*
+Создание inline table-valued функции
+*/
+create function Production.ExpensiveProductsCount (
+	@id int)
+returns table 
+as
+return (
+	select 
+		ProductSubcategoryID,
+		ProductID 
+	from 
+		Production.Product 
+	where 
+		ProductSubcategoryID = @id and StandardCost > 1000);
+
+go
+/*
+Вызов с CROSS APPLY и OUTER APPLY
+*/
+
+select * from Production.Product cross apply Production.ExpensiveProductsCount(ProductSubcategoryID) as E
+order by 
+	E.ProductSubcategoryID;
+
+select * from Production.Product outer apply Production.ExpensiveProductsCount(ProductSubcategoryID) as E
+order by 
+	E.ProductSubcategoryID;
+go
+
+create function Production.ExpensiveProductsCountNew(@id int)
+	returns @expensiveProducts table (
+		ProductSubcategoryID int null,
+		ProductID  int not null
+	)
+as
+begin
+	insert into @expensiveProducts
+	select
+		ProductSubcategoryID,
+		ProductID 
+	from
+		Production.Product 
+	where 
+		ProductSubcategoryID = @id and StandardCost > 1000
+	return
+end;
